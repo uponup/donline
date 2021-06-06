732e8740211e32ee23e39fd93e4840f00a01e118
uponup <1030360567@qq.com>
2021-06-06 22:36:30 +0800

    fix profile utf8

--- fetch.php
+++ fetch.php
@@ -14,7 +14,7 @@ return response(200, $res);
 
 function getPDO() {
     try {
-        $pdo = new PDO("mysql:host='localhost';dbname='donline'", 'donline', 'donline@1407');
+        $pdo = new PDO("mysql:host=localhost;dbname=donline", 'donline', 'donline@1407');
         return $pdo;
     }catch (PDOException $e) {
         echo("数据库链接失败" . $e->getMessage());
